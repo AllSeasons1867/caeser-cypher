c8095ab8c451517764737133aaf8242ab0cced63
FW <35238548+AllSeasons1867@users.noreply.github.com>
2023-09-12 20:22:54 -0600

    Finished basic method structure and input sanitation

--- caeser.rb
+++ caeser.rb
@@ -1,3 +1,12 @@
-def caeser(string) {
-    
-}
+def caeser() 
+    puts "Please give me the sentence you'd like to encrypt."
+    answer = gets.chomp
+
+    if answer.is_a?(String) 
+        puts "You typed out: #{answer}"
+
+        puts "The encypted version: #{answer}"
+    end
+end
+
+caeser()
