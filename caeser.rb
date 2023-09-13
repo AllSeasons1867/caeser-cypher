a1fa66a9406f0a244254f0051c0119bd7c30abab
FW <35238548+AllSeasons1867@users.noreply.github.com>
2023-09-12 21:09:01 -0600

    logic completed with simple tests

--- caeser.rb
+++ caeser.rb
@@ -1,12 +1,17 @@
-def caeser() 
-    puts "Please give me the sentence you'd like to encrypt."
-    answer = gets.chomp
+def caesar_cipher(text, shift)
+    text.chars.map do |char|
+      if char.match(/[A-Za-z]/)
+        base = char.match(/[a-z]/) ? 'a' : 'A'
+        ((char.ord - base.ord + shift) % 26 + base.ord).chr
+      else
+        char
+      end
+    end.join
+  end
+  
+puts caesar_cipher("abc",2)
+puts caesar_cipher("dFa",2)
+puts caesar_cipher("This is a sentence",2)
 
-    if answer.is_a?(String) 
-        puts "You typed out: #{answer}"
 
-        puts "The encypted version: #{answer}"
-    end
-end
 
-caeser()
